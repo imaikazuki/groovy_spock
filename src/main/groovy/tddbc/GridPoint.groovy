e2a4be90a4185ae16ca9b0ee94c0e578fa575e7e
minoru.oonuma <konica.minolta3140@gmail.com>
2015-11-14 12:53:36 +0900

    テストクラスの作成と格子点の生成

---
+++ src/main/groovy/tddbc/GridPoint.groovy
@@ -0,0 +1,7 @@
+package tddbc
+
+/**
+ * Created by numamino on 2015/11/14.
+ */
+class GridPoint {
+}
---
+++ src/test/groovy/tddbc/GridPointSpec.groovy
@@ -0,0 +1,14 @@
+package tddbc
+
+import spock.lang.Specification
+
+/**
+ * Created by numamino on 2015/11/14.
+ */
+class GridPointSpec extends Specification {
+
+    def 格子点が生成できること() {
+        given:
+        def sut = new GridPoint()
+    }
+}
