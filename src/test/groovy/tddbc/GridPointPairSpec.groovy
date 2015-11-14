e83f91cfe48551fe8a2ed47b42210cc1691a6565
minoru.oonuma <konica.minolta3140@gmail.com>
2015-11-14 17:08:24 +0900

    不要なテストコードの削除.

--- src/test/groovy/tddbc/GridPointPairSpec.groovy
+++ src/test/groovy/tddbc/GridPointPairSpec.groovy
@@ -6,12 +6,7 @@ import spock.lang.Specification
  * Created by kazuki on 11/14/15.
  */
 class GridPointPairSpec extends Specification {
-
-    def "格子点のペアが作成できる"(){
-        given:
-        def sut = new GridPointPair(new GridPoint(1, 2), new GridPoint(1, 3))
-    }
-
+    
     def "格子点のペアが,指定した格子点を含むことを判定できる"() {
         given:
         def sut = new GridPointPair(new GridPoint(1, 2), new GridPoint(1, 3))
