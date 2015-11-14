a1ce99c2231ad9f8a2fa7a800de264b00c47808b
IMAI Kazuki <imaikazuki@gmx.com>
2015-11-14 17:03:51 +0900

    格子点のペアが指定した格子点を含むか判定できる

--- src/main/groovy/tddbc/GridPointPair.groovy
+++ src/main/groovy/tddbc/GridPointPair.groovy
@@ -7,8 +7,12 @@ class GridPointPair {
     private GridPoint gp1;
     private GridPoint gp2;
 
-    GridPointPair(GridPoint gp1, GridPoint pg2) {
+    GridPointPair(GridPoint gp1, GridPoint gp2) {
         this.gp1 = gp1
         this.gp2 = gp2
+    }
+
+    def contains(GridPoint opponent) {
+        gp1.hasSameCoordinatesWith(opponent) || gp2.hasSameCoordinatesWith(opponent)
     }
 }
